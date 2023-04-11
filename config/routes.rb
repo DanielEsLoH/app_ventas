7c02d592d09061fcc30e2bc940a0fa670c1f53e5
Daniel Londoño <daniel.esloh@gmail.com>
2023-04-11 07:52:58 -0500

    add the form for the new product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'products#index'
   get '/products/new', to: 'products#new', as: :new_product
   get '/products', to: 'products#index'
   get '/products/:id', to: 'products#show', as: :product
