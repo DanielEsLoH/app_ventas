df8be17e0dc913ed9085672ef76915de79f3d1d4
Daniel Londoño <daniel.esloh@gmail.com>
2023-04-18 18:07:12 -0500

    Add of products to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: :invalid }
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }, format: { with: /\A[a-z-0-9-A-Z]+\z/, message: :invalid }
   validates :password, length: { minimum: 6 }
+  has_many :products, dependent: :destroy
   before_save :downcase_attributes
 
   private
