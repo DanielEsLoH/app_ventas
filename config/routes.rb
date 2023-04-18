5fa2314a47e5470c7fc6cb481dcafef77a559b84
Daniel Londoño <daniel.esloh@gmail.com>
2023-04-17 23:11:33 -0500

    user registration and login created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :categories, except: :show
   resources :products, path: '/'
+
+  namespace :authentication, path: '', as: '' do
+    resources :users, only: %i[new create]
+    resources :sessions, only: %i[new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
